008857d3e991a388c678cebb7efdad973291bbc1
Tomislav Živadinović <tomislav.zivadinovic.3948@metropolitan.ac.rs>
2021-03-04 21:28:22 +0100

    config

---
+++ src/utils/pageMenuItems.ts
@@ -0,0 +1,6 @@
+export const menu: IMenuItem[] = [
+  {title: 'Home', icon: 'home', path: '/home'},
+  {title: 'Invoices', icon: 'assignment_turned_in', path: '/invoices'},
+  {title: 'Proforma Invoices', icon: 'pending_actions', path: '/proforma-invoices'},
+  {title: 'Packing lists', icon: 'inventory_2', path: '/packing-lists'}
+];
